0b5d6dd5e0a0c0b728d12d35add291b916b02b94
Anton Strogonoff <anton@strogonoff.name>
2015-07-21 11:21:41 +0300

    Bumped version from dev to beta

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-upwork-auth',
-    version='1.0-dev',
+    version='1.0-beta',
     packages=['django_upwork_auth'],
     include_package_data=True,
     license='BSD License',
